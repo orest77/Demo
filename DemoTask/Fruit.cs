9d5f3728ef15ba153977a2a7a1883086a4133e89
orest77 <orest.shkhumov@gmail.com>
2018-09-18 17:12:29 +0300

    first commit

---
+++ DemoTask/Fruit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTask
+{
+    class Fruit
+    {
+        private string name;
+        private string color;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public Fruit(string _name , string _color)
+        {
+            this.name = _name;
+            this.color = _color;
+        }
+
+        virtual public void Input()
+        {
+
+        }
+    }
+}
